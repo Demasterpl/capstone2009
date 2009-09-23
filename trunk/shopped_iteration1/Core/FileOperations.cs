b57446d2b57d619ccde1ec2ed57ae2e2c8254432
avanek.cs <avanek.cs@2b0519c2-98a8-11de-bff5-93fd3790126f>
2009-09-23 01:24:39 +0000

    Renamed "CapstoneF2009_Test" project to "UI". Created "Core" project, which will house File I/O, image manipulations, etc.

---
+++ trunk/shopped_iteration1/Core/FileOperations.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Core.Interfaces;
+
+namespace Core
+{
+    public class FileOperations : IFileOperations
+    {
+        public string OpenFile(PictureBox pictureBox)
+        {
+            var openFileDialog = new OpenFileDialog
+             {
+                 InitialDirectory = "c:\\"
+             };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox.Image = new Bitmap(openFileDialog.OpenFile());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+
+            }
+
+            return openFileDialog.FileName;
+        }
+
+        public void SaveFile(PictureBox pictureBox, string fileCurrentlyOpen)
+        {
+            var saveFileDialog = new SaveFileDialog
+             {
+                 FileName = fileCurrentlyOpen
+             };
+            
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox.Image.Save(saveFileDialog.FileName);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ trunk/shopped_iteration1/UI/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using Core;
+using Core.Interfaces;
+
+namespace CapstoneF2009_Test
+{
+    public partial class Form1 : Form
+    {
+        public string CurrentFileName { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developed Fall 2009 at Kent State University\nGroup 4\n\n\nGreg Beca\nAndy Vanek\nDaniel Sheaffer","About Us");
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openPictureButton_Click(object sender, EventArgs e)
+        {
+            IFileOperations fileOperation = new FileOperations();
+            CurrentFileName = fileOperation.OpenFile(PictureBox);
+        }
+
+        private void saveImageButton_Click(object sender, EventArgs e)
+        {
+            IFileOperations fileOperation = new FileOperations();
+            fileOperation.SaveFile(PictureBox, CurrentFileName);
+        }
+
+        private void openPictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IFileOperations fileOperation = new FileOperations();
+            fileOperation.OpenFile(PictureBox);
+        }
+
+        private void savePictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IFileOperations fileOperation = new FileOperations();
+            fileOperation.SaveFile(PictureBox, CurrentFileName);
+        }
+
+
+
+    }
+}
