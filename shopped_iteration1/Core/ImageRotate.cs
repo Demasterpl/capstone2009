03dbc65869e32b840d395019d478f8e81934f801
avanek.cs <avanek.cs@2b0519c2-98a8-11de-bff5-93fd3790126f>
2009-09-24 17:45:23 +0000

    Added new files to repository

---
+++ shopped_iteration1/Core/ImageRotate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Core
+{
+    public class ImageRotate
+    {
+        public Image RotateImageByAngle(Image originalImage, float angle)
+        {
+            var rotatedImage = new Bitmap(originalImage);
+            Graphics g = Graphics.FromImage(rotatedImage);
+
+            //move rotation point to center of image
+            g.TranslateTransform((float)originalImage.Width / 2, (float)originalImage.Height / 2);
+            //rotate
+            g.RotateTransform(angle);
+            //move image back
+            g.TranslateTransform(-(float)originalImage.Width / 2, -(float)originalImage.Height / 2);
+            //draw passed in image onto graphics object
+            g.DrawImage(originalImage, new Point(0, 0));
+            return rotatedImage;
+        }
+    }
+}
---
+++ shopped_iteration1/UI/RotateDialog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class RotateDialog : Form
+    {
+        public float rotateDegrees;
+
+        public RotateDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void RotateDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            rotateDegrees = (float)Convert.ToDouble(RotateTextBox.Text);
+        }
+
+    }
+}
