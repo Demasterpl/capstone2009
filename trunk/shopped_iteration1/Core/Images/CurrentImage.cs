05d70bac5d28f877e9424d94f713b849910b3037
avanek.cs <avanek.cs@2b0519c2-98a8-11de-bff5-93fd3790126f>
2009-09-30 21:29:09 +0000

    Created a new Object called CurrentImage which assists in keeping track of unzoomed, unrotated image. Refactored Rotate and Zoom methods to use CurrentImage. Created several methods (extract - duplicated code). Zoom and Rotate seem to be working correctly now.

---
+++ trunk/shopped_iteration1/Core/Images/CurrentImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Images
+{
+    public class CurrentImage
+    {
+        public int CurrentHeight { get; set; }
+        public int CurrentWidth { get; set; }
+        public int ZoomLevel { get; set; }
+        public float DegreesRotated { get; set; }
+        public Image InitialImage { get; set; }
+
+        public CurrentImage() { }
+        public CurrentImage(int currentHeight, int currentWidth, int zoomLevel, float degreesRotated, Image currentImage)
+        {
+            CurrentHeight = currentHeight;
+            CurrentWidth = currentWidth;
+            ZoomLevel = zoomLevel;
+            DegreesRotated = degreesRotated;
+            InitialImage = currentImage;
+        }
+    }
+}
