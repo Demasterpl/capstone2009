be9e291e6d63cd903f69d6972375eccbb3c4f935
avanek.cs <avanek.cs@2b0519c2-98a8-11de-bff5-93fd3790126f>
2009-10-01 17:46:46 +0000

    Removed Zoom property.

--- trunk/shopped_iteration1/Core/Images/CurrentImage.cs
+++ trunk/shopped_iteration1/Core/Images/CurrentImage.cs
@@ -10,16 +10,14 @@ namespace Core.Images
     {
         public int CurrentHeight { get; set; }
         public int CurrentWidth { get; set; }
-        public int ZoomLevel { get; set; }
         public float DegreesRotated { get; set; }
         public Image InitialImage { get; set; }
 
         public CurrentImage() { }
-        public CurrentImage(int currentHeight, int currentWidth, int zoomLevel, float degreesRotated, Image currentImage)
+        public CurrentImage(int currentHeight, int currentWidth, float degreesRotated, Image currentImage)
         {
             CurrentHeight = currentHeight;
             CurrentWidth = currentWidth;
-            ZoomLevel = zoomLevel;
             DegreesRotated = degreesRotated;
             InitialImage = currentImage;
         }
