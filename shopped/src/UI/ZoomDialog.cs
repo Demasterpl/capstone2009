a3015a6c03103bd0bd4cd89cbfdad2cd5a6c78a5
avanek.cs <avanek.cs@2b0519c2-98a8-11de-bff5-93fd3790126f>
2009-10-27 00:26:58 +0000

    Rearranging the directory structure of the project somewhat (in order to keep Doxygen HTML separate from code). All VS project files are now in the src directory.

---
+++ shopped/src/UI/ZoomDialog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UI
+{
+    /**
+     * The ZoomDialog class displays a small dialog box to prompt the user for
+     * an amount to zoom the image loaded into the editor.
+     * 
+     * @param ZoomLevel Contains the amount of zoom the user specifies.
+     */
+    public partial class ZoomDialog : Form
+    {
+        public float ZoomLevel;
+
+        public ZoomDialog()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * Once the user hits the "Zoom Image" button, this grabs the value from the dialog box.
+         */
+        private void ZoomButton_Click(object sender, EventArgs e)
+        {
+            ZoomLevel = float.Parse(ZoomTextBox.Text) / 100.0f;
+        }
+    }
+}
