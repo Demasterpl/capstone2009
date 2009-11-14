73c8bd114a8c7e10a72afddb256e2a6889b85bbc
avanek.cs <avanek.cs@2b0519c2-98a8-11de-bff5-93fd3790126f>
2009-11-14 21:01:05 +0000

    Added very basic drawing capability. Left click on the image and it will draw a square around the cursor (right now it's a fixed width and color). Should have behavior we discussed in class and should work with undo/redo.

---
+++ shopped/src/Core/Manipulators/ImageDraw.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Core.Manipulators
+{
+    public class ImageDraw
+    {
+        public int LineThickness { get; set; }
+
+        public ImageDraw()
+        {
+            LineThickness = 25;
+        }
+
+        /**
+         * Gets the current position of the cursor over the PictureBox and sets the pixels
+         * around the cursor (within the lineThickness) to the specified color.
+         * 
+         * @param image The image from PictureBox.
+         * @param mouse Contains the current status of the mouse.
+         * 
+         * @return The new image that has been drawn on.
+         */
+        public Image DrawOnImage(Image image, MouseEventArgs mouse)
+        {
+            var tempImage = new Bitmap(image);
+
+            if (mouse.Button == MouseButtons.Left)
+            {
+                var x = mouse.X;
+                var y = mouse.Y;
+                var centerLine = LineThickness / 2;
+
+                for (int i = x - centerLine; i <= x + centerLine; ++i)
+                {
+                    for (int j = y - centerLine; j <= y + centerLine; ++j)
+                    {
+                        try
+                        {
+                            tempImage.SetPixel(i, j, Color.Red);
+                        }
+                        catch
+                        {
+                            //gulp - IndexOutOfBounds Exception
+                        }
+                    }
+                }
+            }
+            return tempImage;
+        }
+    }
+}
